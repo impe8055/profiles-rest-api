60e2d41686405c46f33df61a67ff7a839ce26998
Your Name <you@example.com>
2023-10-27 06:38:03 +0900

    added searching filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -9,6 +9,7 @@ from rest_framework import viewsets
 from profiles_api import models
 
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from profiles_api import permissions
 
 class HelloApiView(APIView):
@@ -104,3 +105,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentications_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
