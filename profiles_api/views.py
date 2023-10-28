8a0a38fabdc0ccd6973cfc3543c2e24ad368ec04
Your Name <you@example.com>
2023-10-29 07:22:12 +0900

    added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -12,6 +12,10 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from profiles_api import permissions
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
@@ -107,3 +111,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+    class UserLoginApiView(ObtainAuthToken):
+        """Create user authentication tokens"""
+        rederer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+        
