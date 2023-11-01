3669cabe76d4974197ae4e39740d991c4b2ebec0
Your Name <you@example.com>
2023-11-01 22:03:00 +0900

    Added Hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '%*e_!x@a^_!ff38h_da#+zmw$!dea6!ai-qj_k4yn%h%l5lio&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-54-66-196-65.ap-southeast-2.compute.amazonaws.com',
+    '127.0.0.1'
+    ]
 
 
 # Application definition
